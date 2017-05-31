64ea224df390e2f9320b0be37eb343aac2a3d32d
Deric Plummer <Deric@Derics-MacBook-Pro.local>
2017-05-31 12:53:01 -0600

    Initial commit

---
+++ app/src/main/java/com/plummer/deric/rubricapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.plummer.deric.rubricapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
