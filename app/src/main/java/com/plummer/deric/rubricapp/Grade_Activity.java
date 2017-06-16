07e1766a62ddb66134fd28880a51e68ac6cc4426
Silver Ninja <mil11032@byui.edu>
2017-06-16 13:25:58 -0600

    added grade activity

---
+++ app/src/main/java/com/plummer/deric/rubricapp/Grade_Activity.java
@@ -0,0 +1,13 @@
+package com.plummer.deric.rubricapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Grade_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_);
+    }
+}
